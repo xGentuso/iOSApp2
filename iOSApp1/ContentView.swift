f5fd6b39e297c70e2fc2b58e2d90acf0f301cba9
xGentuso <ryan.mota@triosstudent.com>
2025-01-23 09:19:27 -0500

    Initial Commit

---
+++ iOSApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp1
+//
+//  Created by ryan mota on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp1/iOSApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp1App.swift
+//  iOSApp1
+//
+//  Created by ryan mota on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
